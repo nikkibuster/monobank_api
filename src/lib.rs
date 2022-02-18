fefab113f2bc285fdc00aae430d7ec2da17e26e6
dethcrock <dethrock@gmail.com>
2022-02-18 20:27:08 +0200

    feature/urls-map

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+use std::collections::HashMap;
+use lazy_static::lazy_static;
+
 pub mod models;
+
+
+lazy_static! {
+    pub static ref URLS: HashMap<&'static str, &'static str> = {
+        let mut map = HashMap::new();
+
+        // "https://api.monobank.ua"
+        map.insert("currency", "https://api.monobank.ua/bank/currency");
+        map
+    };
+}
